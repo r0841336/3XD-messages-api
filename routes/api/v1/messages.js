0b1f173ec4ea4871d12a475c3d91eeb23313f6d9
Dante <r0841336@student.thomasmore.be>
2024-10-04 14:11:10 +0200

    feat: versie terug

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -1,37 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const messages = [
-    { "user": "John", "message": "Hello" },
-    { "user": "Jane", "message": "Hi" }
-];
-
-// Route voor het ophalen van alle berichten
 router.get("/", (req, res, next) => {
+
     res.status(200).json({
         status: "success",
-        message: "GET all messages",
-        data: { message: messages }
-    });
-});
+        "message": "GET messages",
+        "data": {
+            "message": [{
+                "user": "John",
+                "message": "Hello"
+            },
+        {
+            "user": "Jane",
+            "message": "Hi"
+        }]
+        }
 
-// Route voor het ophalen van een specifiek bericht op basis van de index
-router.get("/:index", (req, res, next) => {
-    const index = parseInt(req.params.index, 10);
-
-    // Check of de index binnen de array-limieten valt
-    if (index >= 0 && index < messages.length) {
-        res.status(200).json({
-            status: "success",
-            message: `GET message at index ${index}`,
-            data: { message: messages[index] }
-        });
-    } else {
-        res.status(404).json({
-            status: "fail",
-            message: "Message not found"
-        });
-    }
+    })
 });
 
 module.exports = router;
